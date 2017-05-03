03d7da34f627f0498d23e440c27efdea0f1d9487
Bogumił Kamiński <bkamins@sgh.waw.pl>
2017-05-04 00:55:21 +0200

    tests of core.jl

---
+++ test/core.jl
@@ -0,0 +1,55 @@
+es = EmptyState()
+
+println("Testing core Scheduler creation")
+sReal = Scheduler{EmptyState, Real}(1.2, Vector{EventSimulation.Action{Real}}(), es)
+
+println("Testing if register! and go! produce correct order")
+s = Scheduler()
+
+mlen = 100
+ts = (1.0:mlen)[randperm(mlen)]
+
+for x in ts
+    register!(s, z -> push!(v, z.now), x)
+end
+
+v = Float64[]
+go!(s, mlen)
+@test v == [1.0:mlen;]
+@test length(s.event_queue) == 0
+
+
+println("Testing register! and terminate! for correct number of elements")
+for x in ts
+    register!(s, z -> push!(v, z.now), x)
+end
+
+@test length(s.event_queue) == mlen
+terminate!(s)
+@test length(s.event_queue) == 0
+
+println("Testing ordered bulk_register!")
+s = Scheduler()
+v2 = []
+who = [1:5;]
+for x in randperm(10)
+    bulk_register!(s, who, (a,b) -> push!(v2, (s.now, b)), Float64(x), false)
+end
+go!(s, 11)
+v2t = []
+for i in 1.0:10.0, j in 1:5
+    push!(v2t, (i, j))
+end
+
+@test v2 == v2t
+
+println("Testing random bulk_register!. Visual inspection required.")
+println("First element of tuple should be ordered, second should be in random order")
+s = Scheduler()
+v2 = []
+who = [1:5;]
+for x in randperm(4)
+    bulk_register!(s, who, (a,b) -> println((s.now, b)), Float64(x), true)
+end
+go!(s, 11)
+
