66fdef48139137aa9225307a0115f59e55cba317
Bogumił Kamiński <bkamins@sgh.waw.pl>
2018-03-13 00:32:27 +0100

    fix typo in docstring

--- src/core.jl
+++ src/core.jl
@@ -111,7 +111,7 @@ A convenience constructor of `Scheduler`
 Arguments:
 * `state=EmptyState()` subtype of `AbstractState` to be used by `Scheduler`
 * `T=Float64`          type that will be used to hold time in `Scheduler`
-* `monitor=(s,Δ) -> nothing  monitor function
+* `monitor=(s,Δ) -> nothing`  monitor function
 
 By default an empty `event_queue` is created, `now` is set to `zero(T)`
 and there is idle `monitor`
